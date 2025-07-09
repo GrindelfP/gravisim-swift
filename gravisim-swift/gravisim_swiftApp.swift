458df71176b5f9e2cdae2fad049e768b4ad72a45
GrindelfP <grindelf.perlomutrovij@gmail.com>
2025-07-09 15:02:17 +0300

    Initial Commit

---
+++ gravisim-swift/gravisim_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  gravisim_swiftApp.swift
+//  gravisim-swift
+//
+//  Created by Gregory Shipunov on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct gravisim_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gravisim-swiftTests/gravisim_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  gravisim_swiftTests.swift
+//  gravisim-swiftTests
+//
+//  Created by Gregory Shipunov on 7/9/25.
+//
+
+import Testing
+@testable import gravisim_swift
+
+struct gravisim_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
